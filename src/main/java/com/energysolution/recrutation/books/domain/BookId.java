ec9ac09719723760bfb7d58bbd2b17b9de841d4e
Peace <55354072+nickimilie@users.noreply.github.com>
2024-07-04 14:16:13 +0200

    add generate id

--- src/main/java/com/energysolution/recrutation/books/domain/BookId.java
+++ src/main/java/com/energysolution/recrutation/books/domain/BookId.java
@@ -4,7 +4,10 @@ import com.energysolution.recrutation.common.framework.Identifier;
 import java.util.UUID;
 import lombok.Value;
 
+/**
+ * Books Identifier
+ */
 @Value
 public class BookId implements Identifier {
-  UUID id;
+  UUID id = UUID.randomUUID();
 }
