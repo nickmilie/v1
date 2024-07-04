7437b07d1511646590092ef3e65d3b4a1ab82794
Peace <55354072+nickimilie@users.noreply.github.com>
2024-07-04 14:13:07 +0200

    entity

--- src/main/java/com/energysolution/recrutation/books/domain/Book.java
+++ src/main/java/com/energysolution/recrutation/books/domain/Book.java
@@ -1,18 +1,36 @@
 package com.energysolution.recrutation.books.domain;
 
 import com.energysolution.recrutation.common.framework.IdentifiableAggregateRoot;
-import jakarta.persistence.EmbeddedId;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.time.LocalDateTime;
+
+/**
+ * Main entity in books domain
+ */
 @Getter
+@Setter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
 @Table(name = "BOOKS")
 public class Book implements IdentifiableAggregateRoot<BookId> {
 
-  @EmbeddedId private BookId id;
+  @EmbeddedId
+  private BookId id;
+
+  @Column
+  private String title;
+
+  @Column
+  private String author;
+
+  @Column
+  private String ISBN;
+
+  @Column
+  private LocalDateTime publicationDate;
 }
