b53a9dee09a6405e0c076d8d60e73c07fe33df28
Peace <55354072+nickimilie@users.noreply.github.com>
2024-07-04 14:16:39 +0200

    add repository annotation

--- src/main/java/com/energysolution/recrutation/books/domain/BookRepository.java
+++ src/main/java/com/energysolution/recrutation/books/domain/BookRepository.java
@@ -1,6 +1,11 @@
 package com.energysolution.recrutation.books.domain;
 
 import com.energysolution.recrutation.common.framework.CommonRepository;
+import org.springframework.stereotype.Repository;
 
+/**
+ * Repository for books domain
+ */
+@Repository
 public interface BookRepository extends CommonRepository<Book, BookId> {
 }
